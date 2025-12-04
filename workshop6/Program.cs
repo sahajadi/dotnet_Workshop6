6f308e0d2aa791521149bcbc7ce8b34e5404756f
Sahajadi <sahajadilohani123@gmail.com>
2025-12-04 13:09:06 +0545

    Add project files.

---
+++ workshop6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace workshop6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a rectangle object
+            Rectangle rect = new Rectangle
+            {
+                Length = 20,
+                Breadth = 10
+            };
+
+            // Display results
+            Console.WriteLine(rect.ShowDetails());
+            Console.WriteLine($"Area: {rect.GetArea()}");
+            Console.WriteLine($"Perimeter: {rect.GetPerimeter()}");
+        }
+    }
+}
---
+++ workshop6/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop6
+{
+    internal class Rectangle
+    {
+        private double length;
+        private double breadth;
+
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public double Breadth
+        {
+            get => breadth;
+            set => breadth = value;
+        }
+
+        // Expression-bodied method for area
+        public double GetArea() => length * breadth;
+
+        // NEW: Expression-bodied method for perimeter
+        public double GetPerimeter() => 2 * (length + breadth);
+
+        // Expression-bodied method for showing details
+        public string ShowDetails() => $"Length: {length}, Breadth: {breadth}";
+    }
+}
