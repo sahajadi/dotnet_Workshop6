4ca969b13331fef296cb06991746ee3cfc17b65d
Sahajadi <sahajadilohani123@gmail.com>
2025-12-04 13:41:31 +0545

    Task 3 completed

---
+++ workshop6/NumberProcessor.cs
@@ -0,0 +1,13 @@
+﻿public class NumberProcessor
+{
+    public static void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+    {
+        foreach (var num in numbers)
+        {
+            if (condition(num))
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
--- workshop6/Program.cs
+++ workshop6/Program.cs
@@ -19,17 +19,37 @@ namespace workshop6
 
 
             // ---------- TASK 2.1, 2.2, 2.3 ----------
-            Console.WriteLine("\n--- Task 2: Custom Delegate Extended ---");
+            //Console.WriteLine("\n--- Task 2: Custom Delegate Extended ---");
 
-            double price = 1000;
+            //double price = 1000;
 
-            // Using existing discount methods
-            Console.WriteLine($"Festival Final Price: {DiscountService.CalculateFinalPrice(price, DiscountService.FestivalDiscount)}");
-            Console.WriteLine($"Seasonal Final Price: {DiscountService.CalculateFinalPrice(price, DiscountService.SeasonalDiscount)}");
-            Console.WriteLine($"No Discount Final Price: {DiscountService.CalculateFinalPrice(price, DiscountService.NoDiscount)}");
+            //// Using existing discount methods
+            //Console.WriteLine($"Festival Final Price: {DiscountService.CalculateFinalPrice(price, DiscountService.FestivalDiscount)}");
+            //Console.WriteLine($"Seasonal Final Price: {DiscountService.CalculateFinalPrice(price, DiscountService.SeasonalDiscount)}");
+            //Console.WriteLine($"No Discount Final Price: {DiscountService.CalculateFinalPrice(price, DiscountService.NoDiscount)}");
 
-            // Lambda expression for 30% discount (Task 2.3)
-            Console.WriteLine($"30% Discount Final Price: {DiscountService.CalculateFinalPrice(price, p => p * 0.70)}");
+            //// Lambda expression for 30% discount (Task 2.3)
+            //Console.WriteLine($"30% Discount Final Price: {DiscountService.CalculateFinalPrice(price, p => p * 0.70)}");
+
+
+            //Task 3
+
+            Console.WriteLine("\n--- Task 3: Built-in Delegate ---");
+
+            int[] nums = { 2, 5, 12, 7, 18, 3, 22 };
+
+            // Only even numbers
+            Console.WriteLine("Even Numbers:");
+            NumberProcessor.ProcessNumbers(nums, n => n % 2 == 0);
+
+            // Only numbers greater than 10
+            Console.WriteLine("\nNumbers > 10:");
+            NumberProcessor.ProcessNumbers(nums, n => n > 10);
+
+
+
+
         }
+
     }
 }
